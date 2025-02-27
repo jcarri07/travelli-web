412bb588eb927ebf20c332d3ebc813e1a4b55cbc
Jose Chourio <chou@Joses-MacBook-Pro.local>
2025-02-26 22:23:08 -0400

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
         <Route path="/privacy-policies" element={<PrivacyPolicies />} />
         <Route path="/how-to-delete-account" element={<HowToDelete />} />
         <Route path="/plans" element={<PricingPlans />} />
-        <Route path="/app/:id" element={<TripDeepLinkRedirect />}/>
+        <Route path="/trip/app/:id" element={<TripDeepLinkRedirect />}/>
       </Routes>
       <Footer userType={userType}/>
     </div>
